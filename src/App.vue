be0bb38bd46290cc1e229256a8f4d9db910c25a6
ARRAY-LIST <1348909544@qq.com>
2024-01-09 22:06:25 +0800

    内网穿透

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <h1>内网穿透工具</h1>
+      <p>https://natapp.cn/</p>
+      <p>https://www.cpolar.com/</p>
     </nav>
     <router-view/>
   </div>
